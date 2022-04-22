71c8943ebf140bfda6a3e1cdbfe95855393ce343
travisyx <travisyx@gmail.com>
2022-04-21 22:06:51 -0700

    Resized images to 50x50

--- process.py
+++ process.py
@@ -1,3 +1,4 @@
+# Test file to process the images
 import cv2
 import numpy as np
 import pandas as pd
@@ -6,15 +7,13 @@ import matplotlib as plt
 PATH = '/groups/CS156b/data/'
 
 fil = pd.read_csv("train.csv")
-img = cv2.imread(PATH + str(fil['Path'][0]))
-height = img.shape[0] 
-width = img.shape[1] 
-channels = img.shape[2]
-print(height,width,channels)
-new_img = cv2.resize(img, (50,50))
-
-height = new_img.shape[0] 
-width = new_img.shape[1] 
-channels = new_img.shape[2]
-print(height,width,channels)
-cv2.imwrite("test_resize.png", new_img)
+print(len(fil['Path']))
+cnt = 0
+for path in fil['Path']:
+    if cnt % 1000 == 0:
+        print(cnt)
+    img = cv2.imread(PATH + path)
+    new_img = cv2.resize(img, (50,50))
+    pth = path.replace("/","_")
+    cv2.imwrite(f"resized_images/{pth}", new_img)
+    cnt += 1
