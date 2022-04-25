490daefbf45c9c24980ca5c877290ce4c4222f1f
Derek Ing <75998934+dereking1@users.noreply.github.com>
2022-04-24 20:08:21 -0700

    64x64

--- cnn.py
+++ cnn.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.image as mpimg
-import matplitlib.pyplot as plt
+import matplotlib.pyplot as plt
 from PIL import Image
 import torch
 from torch import nn
@@ -15,14 +15,16 @@ model = nn.Sequential(
     nn.MaxPool2d(2),
     nn.Dropout(p=0.1),
     nn.Flatten(),
-    nn.Linear(25 * 16, 100),
-    nn.Linear(100, 14)
+    nn.Linear(3600, 100),
+    nn.Linear(100, 14),
+    nn.Tanh(),
 )
 
-criterion = nn.CrossEntropyLoss()
+criterion = nn.MSELoss()
 optimizer = optim.RMSprop(model.parameters())
 
 def train(trainloader, n_epochs=10):
+    model.to(cuda0)
     training_loss_history = np.zeros([n_epochs, 1])
 
     for epoch in range(n_epochs):
@@ -33,9 +35,10 @@ def train(trainloader, n_epochs=10):
         model.train()
         for i, data in enumerate(trainloader):
             image, labels = data
+            image, labels = image.cuda(), labels.cuda()
             optimizer.zero_grad()
             # forward pass
-            output = model(images)
+            output = model(image)
             # calculate categorical cross entropy loss
             loss = criterion(output, labels)
             # backward pass
@@ -45,15 +48,37 @@ def train(trainloader, n_epochs=10):
             # track training loss
             training_loss_history[epoch] += loss.item()
 
-        training_loss_history[epoch] /= len(training_data_loader)
-        print(f'\n\tloss: {training_loss_history[epoch,0]:0.4f}',end='')
-    
+        training_loss_history[epoch] /= len(trainloader)
+        print('\n\tloss: ' + str(training_loss_history[epoch,0]))
+
     return training_loss_history
+
+def predict(model, testloader):
+    preds = np.zeros((len(testloader),14))
+    model.to(cuda0)
+    with torch.no_grad():
+        model.eval()
+        for i, data in enumerate(testloader):
+            if i == 0:
+                print(data)
+            image = data.cuda()
+            # forward pass
+            output = model(image)
+            if i == 1:
+                print(output.data.size())
+            preds[i,:] = output.data.cpu().numpy()
+    return preds
 
 def main():
     X, Y = get_train_data()
     _, train_loader = trainload(X, Y)
+    #testloader = get_test_data()
+    
     losses = train(train_loader)
+    #preds = predict(model, testloader)
+    #output = pd.DataFrame(preds, columns=label_columns)
+    #output.insert(0, 'Id', testdf['Id'])
+    #output.to_csv('/home/ding/testpreds.csv')
     plt.figure()
     plt.plot(range(1,11),losses)
     plt.savefig('/home/ding/testtrain.png')
--- data.py
+++ data.py
@@ -21,33 +21,60 @@ label_columns = ['No Finding', 'Enlarged Cardiomediastinum', 'Cardiomegaly','Lun
 data_path = '/central/groups/CS156b/data/'
 train_path = data_path + 'student_labels/train.csv'
 test_path = data_path + 'student_labels/test_ids.csv'
-imagesize = (50,50)
+imagesize = (64,64)
 
 cuda0 = torch.device('cuda:0')
 
-def train_df():
-    return pd.read_csv(train_path)
-
 def get_train_data():
-    df = train_df().fillna(0)
+    df = pd.read_csv(train_path).fillna(0)
     X = []
     Y = []
 
     for index, row in df.iterrows():
+        if index == int(df.shape[0]/100):
+            break
         image_path = data_path + row['Path']
 
-        image = Image.open(image_path).resize(imagesize)
-        image_gray = ImageOps.grayscale(image)
+        image = Image.open(image_path)
+        imcopy = image.copy()
+        #.resize(imagesize)
+        image_gray = ImageOps.grayscale(imcopy)
 
-        X.append(image_gray)
+        X.append(imcopy)
+        image.close()
         Y.append(row[label_columns])
     return X, Y
 
-def trainload(X, Y):
+def get_test_data():
+    df = pd.read_csv(test_path)
+    X = []
+    for index, row in df.iterrows():
+        image_path = data_path + row['Path']
+
+        image = Image.open(image_path)
+        #.resize(imagesize)
+        #image_gray = ImageOps.grayscale(image)
+
+        X.append(image)
+    trans = T.Compose([T.ToTensor(), T.Resize(size=(64,64))])
     for i in range(len(X)):
-        X[i] = T.ToTensor(X[i])
-    trainx = torch.Tensor(X, dtype=torch.float32, device=cuda0)
-    trainy = torch.Tensor(Y, dtype=torch.float32, device=cuda0)
+       X[i] = trans(X[i]).numpy()
+    testx = torch.from_numpy(np.array(X).astype(np.float32))
+    testx.to(cuda0)
+
+    testloader = DataLoader(testx, batch_size=1, shuffle=False)
+    return testloader
+
+def trainload(X, Y):
+    trans = T.Compose([T.ToTensor(), T.Resize(size=(64,64))])
+    for i in range(len(X)):
+       X[i] = trans(X[i]).numpy()
+    #trainx = torch.Tensor(X).cuda(device=cuda0)
+    #trainy = torch.Tensor(Y).cuda(device=cuda0)
+    trainx = torch.from_numpy(np.array(X).astype(np.float32))
+    trainy = torch.from_numpy(np.array(Y).astype(np.float32))
+    trainx.to(cuda0)
+    trainy.to(cuda0)
 
     traindata = data_utils.TensorDataset(trainx, trainy)
     train_loader = DataLoader(traindata, batch_size=256, shuffle=True)
