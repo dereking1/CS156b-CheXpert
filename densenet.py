2e50c3a1841919fa9f1c5e1594edf2583a35f9fd
Derek Ing <75998934+dereking1@users.noreply.github.com>
2022-05-08 21:39:56 -0700

    mb havent updated in a while

--- densenet.py
+++ densenet.py
@@ -8,32 +8,31 @@ import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
+import torchvision.models as models
 import torch.hub
 from collections import OrderedDict
+import time
+import math
 
-state_dict = torch.hub.load_state_dict_from_url('https://download.pytorch.org/models/densenet121-a639ec97.pth',progress=False)
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
 
-class DenseNet121(nn.Module):
-    def __init__(self, out_size):
-        super(DenseNet121, self).__init__()
-        self.densenet121 = torchvision.models.densenet121(memory_efficient=True)
-        num_ftrs = self.densenet121.classifier.in_features
-        self.densenet121.classifier = nn.Sequential(
-            nn.Linear(num_ftrs, out_size),
+model = models.densenet121(memory_efficient=True, pretrained=True)
+model.features.conv0 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+model.classifier = nn.Sequential(
+            nn.Linear(1024, 14),
             nn.Tanh()
         )
-
-    def forward(self, x):
-        x = self.densenet121(x)
-        return x
-
-model = DenseNet121(out_size=14)
 criterion = nn.MSELoss()
-optimizer = optim.RMSprop(model.parameters())
+optimizer = optim.Adam(model.parameters())
 
 def train(trainloader, n_epochs=10):
-    model.cuda()
     model.train()
+    model.cuda()
     training_loss_history = np.zeros([n_epochs, 1])
 
     for epoch in range(n_epochs):
@@ -63,6 +62,7 @@ def predict(model, testloader):
     preds = np.zeros((len(testloader),14))
     with torch.no_grad():
         model.eval()
+        model.cuda()
         for i, data in enumerate(testloader):
             image = data.cuda()
             # forward pass
@@ -71,22 +71,24 @@ def predict(model, testloader):
     return preds
 
 def main():
-    X, Y = get_train_data()
-    _, train_loader = trainload(X, Y)
-    #testloader = get_test_data()
+    #X, Y = get_train_data()
+    #train_loader = trainload(X, Y)
+    train_loader = get_train_data()
+    #print('data loaded')
+    testloader = get_test_data()
     
     n_epochs = 100
     losses = train(train_loader, n_epochs=n_epochs)
-    #preds = predict(model, testloader)
+    preds = predict(model, testloader)
     
-    #testdf = pd.read_csv(test_path)
-    #output = pd.DataFrame(preds, columns=label_columns)
-    #output.insert(0, 'Id', testdf['Id'])
-    #output.to_csv('/home/ding/densepreds.csv')
+    testdf = pd.read_csv(test_path)
+    output = pd.DataFrame(preds, columns=label_columns)
+    output.insert(0, 'Id', testdf['Id'])
+    output.to_csv('/home/ding/densepreds_dia_edge.csv')
     
     plt.figure()
     plt.plot(range(1,n_epochs+1),losses)
-    plt.savefig('/home/ding/testdense.png')
+    plt.savefig('/home/ding/testdense_dia_edge.png')
 
 if __name__ == '__main__':
     main()
